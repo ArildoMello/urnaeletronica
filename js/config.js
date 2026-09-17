d50c3bdd48c6d47202c5c7c89a709830c4e3f030
Arildo Mello <arildomello@users.noreply.github.com>
2026-09-17 20:21:31 -0300

    feat: conecta urna à planilha de votos

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  appScriptUrl: 'https://script.google.com/macros/s/AKfycbwuqajxtPmr5ZjrpoJi1qKkU1QCPt3O_5-bgCpiWAxx7ny1VOu7yEiKQkRcirD51Cnj/exec',
+  voteAccessKey: 'banana-urna-2026',
+};
